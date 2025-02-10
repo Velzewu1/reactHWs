c5248977bd744e3e428363375de10bf60711534c
Саматов Ансар Нурланоаич <171016665@step.kz>
2025-02-10 20:39:52 +0500

    Task2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import './App.css';
 import { useState } from 'react';
 import Counter from './components/Counter' 
+import UserInfo from './components/UserInfo' 
 
 function App() {
-  const [clickCount, setclickCount] = useState(0) 
+  const [clickCount, setclickCount] = useState(0);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
 
   function handleClick(){
     return setclickCount(clickCount + 1);
+  }
+  
+  function handleName(event){
+    return setName(event.target.value);
+  }
+  function handleAge(event){
+    return setAge(event.target.value);
   }
 
   return (
     <>
       <Counter onClick={handleClick}/>
       <h1>{clickCount}</h1>
+
+      <hr/>
+
+      <form>
+        <h1>Name:</h1>
+        <input type="text" value={name} onChange={handleName}></input>
+        <h1>Age:</h1>
+        <input type='number' value={age} onChange={handleAge}></input>
+      </form>
+
+      <UserInfo name={name} age={age}></UserInfo>
     </>
   );
 }
