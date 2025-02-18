e69a68a62290ba75211fc483dbfc81031e968fd5
ayo <150446051+ayan1232008@users.noreply.github.com>
2025-02-18 20:37:29 +0500

    /
    /

---
+++ greetings/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CourseInfo from "./CourseInfo"; 
+
+function App() {
+  return (
+    <div>
+      <CourseInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ greetings/src/CourseInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CourseInfo.css"; 
+
+const CourseInfo = () => {
+  return (
+    <div className="course-info">
+      <h2>React Course</h2>
+      <p>Это крутой курс по реакту</p>
+      <button className="learn-more">узнать больше</button>
+    </div>
+  );
+};
+
+export default CourseInfo;
